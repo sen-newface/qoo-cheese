5b2072cfbab6accf6f52c71eade39f4a4c25f20e
kamata <kamata@SENPC-0616.local>
2020-05-12 11:12:28 +0900

    APIルーティングの定義（コントローラはまだ作成していない）

--- yeahcheese/routes/api.php
+++ yeahcheese/routes/api.php
@@ -3,4 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('user', 'Api\UserController@index');
+Route::post('register', 'Api\UserController@register');
+Route::post('login', 'Api\UserController@login');
+Route::get('logout', 'Api\UserController@logout');
+
+Route::group(['prefix' => 'events'], function() {
+    Route::get('{page}', 'Api\EventController@index');
+    Route::post('auth', 'Api\EventController@auth');
+    Route::get('{id}', 'Api\EventController@show');
+    Route::post('/', 'Api\EventController@store');
+    Route::put('{id}', 'Api\EventController@update');
+    Route::delete('{id}', 'Api\EventController@delete');
+});
 
