8d16090451352351ee41248b9d9f86b710d4011f
kamata <kamata@SENPC-0616.local>
2020-05-12 13:03:50 +0900

    認証後の写真一覧（authメソッド）の追加

--- yeahcheese/routes/api.php
+++ yeahcheese/routes/api.php
@@ -8,7 +8,7 @@ Route::post('register', 'Api\UsersController@register');
 Route::post('login', 'Api\UsersController@login');
 Route::get('logout', 'Api\UsersController@logout');
 
+Route::post('events/auth', 'Api\EventsController@auth')->name('events.auth');
+
 Route::resource('events', 'Api\EventsController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
 Route::resource('events.photos', 'Api\PhotosController', ['only' => ['index', 'store', 'destroy']]);
-
-
