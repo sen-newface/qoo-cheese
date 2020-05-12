6005d481e3e9ef4051652ff391b45864b2dbbd0a
kamata <kamata@SENPC-0616.local>
2020-05-12 13:12:29 +0900

    必要なコントローラを定義

---
+++ yeahcheese/app/Http/Controllers/Api/PhotosController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PhotosController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+    public function store()
+    {
+        //
+    }
+    public function delete()
+    {
+        //
+    }
+}
---
+++ yeahcheese/app/Http/Controllers/Api/UsersController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+
+    public function login()
+    {
+        //
+    }
+
+    public function logout()
+    {
+        //
+    }
+
+}
