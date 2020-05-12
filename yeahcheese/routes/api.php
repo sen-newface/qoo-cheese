7b23ce10e38e4a1936cee2364f80c76ab8967aa7
kamata <kamata@SENPC-0616.local>
2020-05-12 15:40:04 +0900

    UsersControllerはLaravel標準のものを使用するので削除（コントローラの作成は後々やるので、とりあえずこれでルーティング定義は終了）

--- yeahcheese/routes/api.php
+++ yeahcheese/routes/api.php
@@ -3,11 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('user', 'Api\UsersController@index');
-Route::post('register', 'Api\UsersController@register');
-Route::post('login', 'Api\UsersController@login');
-Route::get('logout', 'Api\UsersController@logout');
-
 Route::post('events/auth', 'Api\EventsController@auth')->name('events.auth');
 
 Route::resource('events', 'Api\EventsController', ['only' => ['index', 'show', 'store', 'update', 'destroy']]);
