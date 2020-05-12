6d07b4649e78d02dab8c86232b81db6e9cfb2269
kamata <kamata@SENPC-0616.local>
2020-05-12 17:35:31 +0900

    resource/js/storeフォルダ作成後、配下にストアを作成するindex.jsを置き、Vueのエントリポイントに読み込ませた（app.js）

--- yeahcheese/resources/js/app.js
+++ yeahcheese/resources/js/app.js
@@ -1,12 +1,12 @@
 require('./bootstrap');
 import Vue from 'vue';
 import Vuex from 'vuex';
+import store from './store';
 import App from './app.vue';
-
-Vue.use(Vuex);
 
 const app = new Vue ({
     el: '#app',
+    store,
     components: { App },
     template: '<App/>'
 });
