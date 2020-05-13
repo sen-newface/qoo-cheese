1a9f607a2d3caf0687c2c275e129a11606794fac
y-kazuya <y.kazuya.0124@au.com>
2020-05-13 17:35:43 +0900

    user モデル token周り設定

--- yeahcheese/app/User.php
+++ yeahcheese/app/User.php
@@ -5,10 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-  use Notifiable;
+  use HasApiTokens, Notifiable;
 
   /**
    * イベントの写真を取得
@@ -44,4 +45,10 @@ class User extends Authenticatable
   protected $casts = [
     'email_verified_at' => 'datetime',
   ];
+
+  public function setToken($key = "authToken")
+  {
+    $token = $this->createToken($key)->plainTextToken;
+    $this->token = $token;
+  }
 }
