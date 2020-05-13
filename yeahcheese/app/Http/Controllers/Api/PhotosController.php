32a7586208cfc8a870f3a6e48df3442392bd628e
kamata <kamata@SENPC-0616.local>
2020-05-13 15:57:13 +0900

    写真コントローラがやるべきこと（メソッド）をコメント

--- yeahcheese/app/Http/Controllers/Api/PhotosController.php
+++ yeahcheese/app/Http/Controllers/Api/PhotosController.php
@@ -7,10 +7,21 @@ use Illuminate\Http\Request;
 
 class PhotosController extends Controller
 {
+    /**
+     * イベントに紐づく写真の追加
+     *
+     * @return void
+     */
     public function store()
     {
         //
     }
+
+    /**
+     * イベントに紐づく写真の削除
+     *
+     * @return void
+     */
     public function delete()
     {
         //
