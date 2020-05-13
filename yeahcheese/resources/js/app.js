328f8338aafeaaae4f45de23665e5aac892ca9d4
kamata <kamata@SENPC-0616.local>
2020-05-13 14:04:38 +0900

    app.jsでrouterを登録し、アプリケーションで使用できるようにした

--- yeahcheese/resources/js/app.js
+++ yeahcheese/resources/js/app.js
@@ -1,9 +1,11 @@
 require('./bootstrap');
 import Vue from 'vue';
+import router from './router';
 import App from './app.vue';
 
 const app = new Vue ({
     el: '#app',
+    router,
     components: { App },
     template: '<App/>'
 });
