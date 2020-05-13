c5fec0c7dc8fb241c93e6e6b14f3a1879af53e5e
kamata <kamata@SENPC-0616.local>
2020-05-13 12:19:24 +0900

    ストアをVueインスタンスに追加

--- yeahcheese/resources/js/app.js
+++ yeahcheese/resources/js/app.js
@@ -1,9 +1,11 @@
 require('./bootstrap');
 import Vue from 'vue';
+import store from './store';
 import App from './app.vue';
 
 const app = new Vue ({
     el: '#app',
+    store,
     components: { App },
     template: '<App/>'
 });
