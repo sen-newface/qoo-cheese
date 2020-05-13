537e1b58aa3556f514c397acbdae5b89aee2380a
kamata <kamata@SENPC-0616.local>
2020-05-13 15:53:36 +0900

    ユーザーコントローラがやるべきこと（メソッド）をコメント

--- yeahcheese/app/Http/Controllers/Api/UsersController.php
+++ yeahcheese/app/Http/Controllers/Api/UsersController.php
@@ -7,21 +7,41 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    /**
+     * ユーザー取得
+     *
+     * @return void
+     */
     public function index()
     {
-        //
+        
     }
 
+    /**
+     * アカウント作成
+     *
+     * @return void
+     */
     public function register()
     {
         //
     }
 
+    /**
+     * ログイン
+     *
+     * @return void
+     */
     public function login()
     {
         //
     }
 
+    /**
+     * ログアウト
+     *
+     * @return void
+     */
     public function logout()
     {
         //
