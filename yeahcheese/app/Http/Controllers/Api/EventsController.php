3eed66b1f0c87236e72ec8e22a9c877a2d0a6c3d
kamata <kamata@SENPC-0616.local>
2020-05-13 16:01:00 +0900

    イベントコントローラがやるべきこと（メソッド）をコメント

--- yeahcheese/app/Http/Controllers/Api/EventsController.php
+++ yeahcheese/app/Http/Controllers/Api/EventsController.php
@@ -7,26 +7,62 @@ use Illuminate\Http\Request;
 
 class EventsController extends Controller
 {
+    /**
+     * イベント一覧取得
+     *
+     * @return void
+     */
     public function index()
     {
         //
     }
+
+    /**
+     * 認証キーと紐づくイベント取得
+     * ? これは別コントローラに切り分けたほうがスッキリする？ 
+     * 
+     * @return void
+     */
     public function auth()
     {
         //
     }
+
+    /**
+     * 単一イベント取得
+     *
+     * @return void
+     */
     public function show()
     {
         //
     }
+
+    /**
+     * イベント追加
+     *
+     * @return void
+     */
     public function store()
     {
         //
     }
+
+    /**
+     * イベント情報更新（写真を除く）
+     *
+     * @return void
+     */
     public function update()
     {
         //
     }
+
+    /**
+     * イベント削除
+     *
+     * @return void
+     */
     public function destroy()
     {
         //
