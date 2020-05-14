06bd2a67aba7abd5a4f5d0e8b15c68847d7f55fd
kamata <kamata@SENPC-0616.local>
2020-05-14 17:11:21 +0900

    イベント詳細コンポーネントを作成（propsでイベント渡します）

---
+++ yeahcheese/resources/js/BaseEventDetail.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="base-event-detail" class="container">
+        <p class="event-name">イベント名：{{ eventInfo.name }}</p>
+        <p class="auth-key">認証キー：{{ eventInfo.key }}</p>
+        <p class="event-date">期限：{{ eventInfo.start_date }}-{{ eventInfo.end_date }}</p>
+        <div class="event-photos">
+            <div 
+                v-for="photo in eventInfo.photos"
+                :key="photo.id"
+                class="event-photo"
+             >
+                {{ photo.name }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        eventInfo: {
+            type: Object
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
