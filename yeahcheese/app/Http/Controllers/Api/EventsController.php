ac9445844af2984c5e649291c6189eec93442e1a
kamata <kamata@SENPC-0616.local>
2020-05-14 15:25:42 +0900

    不要な改行を削除

--- yeahcheese/app/Http/Controllers/Api/EventsController.php
+++ yeahcheese/app/Http/Controllers/Api/EventsController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Event;
-
 use App\Http\Resources\Event as EventResource;
 
 class EventsController extends Controller
