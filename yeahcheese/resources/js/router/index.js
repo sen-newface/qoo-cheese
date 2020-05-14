38f40258213e0faafba74b55c2525a8881c1b799
kamata <kamata@SENPC-0616.local>
2020-05-14 15:16:23 +0900

    ルート定義をし、コンポーネントを表示できるように設定

--- yeahcheese/resources/js/router/index.js
+++ yeahcheese/resources/js/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Index from '../pages/index.vue';
+import E401 from '../pages/401.vue';
+import E403 from '../pages/403.vue';
+import E404 from '../pages/404.vue';
+import E500 from '../pages/500.vue';
 
 Vue.use(Router);
 
@@ -9,6 +13,22 @@ const routes = [
   {
     path: '/',
     component: Index
+  },
+  {
+    path: '/401',
+    component: E401
+  },
+  {
+    path: '/403',
+    component: E403
+  },
+  {
+    path: '/404',
+    component: E404
+  },
+  {
+    path: '/500',
+    component: E500
   }
 ];
 
