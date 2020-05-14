5dbc69fb7b306285766e0aec021b00d2610bb6af
kamata <kamata@SENPC-0616.local>
2020-05-14 10:44:27 +0900

    ミューテーション定義を各モジュールのmutations.jsにインポートし、使い方のサンプルを記述

---
+++ yeahcheese/resources/js/store/users/mutations.js
@@ -0,0 +1,9 @@
+import * as types from '../mutation-types';
+
+export default {
+  // TODO: ステート内のイベントを更新する処理
+  // TODO: dispatch(types.SET_USER, payload) で、このミューテーションが呼び出される
+  // [types.SET_EVENT](state, payload) {
+  // },
+  // ... 
+}
