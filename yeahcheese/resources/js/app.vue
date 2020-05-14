389c0b15a23fe923f30b1f78e19436c3513528a0
kamata <kamata@SENPC-0616.local>
2020-05-14 11:39:54 +0900

    ヘッダーコンポーネントを作成し、app.vueに読み込む

--- yeahcheese/resources/js/app.vue
+++ yeahcheese/resources/js/app.vue
@@ -1,12 +1,17 @@
 <template>
     <div id="app">
+        <common-header></common-header>
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+import CommonHeader from './components/CommonHeader.vue';
 export default {
     name: 'App',
+    components: {
+        CommonHeader
+    }
 }
 </script>
 
---
+++ yeahcheese/resources/js/components/CommonHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header id="header">
+        aaaa
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'CommonHeader'
+}
+</script>
+
+<style scoped>
+
+</style>
