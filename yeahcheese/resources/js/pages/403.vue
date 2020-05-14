6c4d3dd4913b1e5c855ce9cca22d758f18d831cf
kamata <kamata@SENPC-0616.local>
2020-05-14 15:18:47 +0900

    各エラーに対するコンポーネントを作成

---
+++ yeahcheese/resources/js/pages/403.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- デザインは後々修正する -->
+    <div class="error-container alert alert-secondary" role="alert">
+        このコンテンツを閲覧する権限を持っていません
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'E401'    
+}
+</script>
+
+<style>
+.error-container {
+    padding: 10%;
+}
+</style>
