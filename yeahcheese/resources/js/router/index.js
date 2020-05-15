eb7568668e263c1895cf8e2da5cd7ad592a5e850
y-kazuya <y.kazuya.0124@au.com>
2020-05-15 12:50:48 +0900

    ユーザー登録ページへの ルーティング定義 ログイン時にトップページへ飛ぶルートがーどの定義

--- yeahcheese/resources/js/router/index.js
+++ yeahcheese/resources/js/router/index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import store from '../store';
 import Index from '../pages/index.vue';
+import Register from '../pages/register.vue'
 
 Vue.use(Router);
 
@@ -9,12 +11,24 @@ const routes = [
   {
     path: '/',
     component: Index
+  },
+  {
+    path: '/register',
+    component: Register,
+    meta: { requiresNotAuth: true }
   }
 ];
 
 const router = new Router({
   mode: 'history',
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  // if (to.matched.some(record => record.meta.requiresNotAuth) && store.getters['users/isLogin']) {
+  //   next({ path: '/' });
+  // }
+  next();
 });
 
 export default router;
