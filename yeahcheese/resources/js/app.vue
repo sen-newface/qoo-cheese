7381f2f0df774d47f404ec58945ba007165a3e47
y-kazuya <y.kazuya.0124@au.com>
2020-05-15 12:51:47 +0900

    不要記述の削除

--- yeahcheese/resources/js/app.vue
+++ yeahcheese/resources/js/app.vue
@@ -1,16 +1,14 @@
 <template>
   <div id="app">
-    <router-view></router-view>
-    <testAuth></testAuth>
+    <div class="container mt-5">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-//add_auth_apiのマージが完了後 testAuth関連は削除
-import testAuth from "./tmpAuthTest";
 export default {
-  name: "App",
-  components: { testAuth }
+  name: "App"
 };
 </script>
 
