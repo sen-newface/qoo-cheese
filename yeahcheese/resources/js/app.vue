d779284305a2841d1a970870016754684be73ed6
y-kazuya <y.kazuya.0124@au.com>
2020-05-15 15:31:08 +0900

    header 取り込み

--- yeahcheese/resources/js/app.vue
+++ yeahcheese/resources/js/app.vue
@@ -1,20 +1,20 @@
 <template>
   <div id="app">
-      <common-header></common-header>
-      <div class="container mt-5">
-        <router-view></router-view>
-      </div>
+    <common-header></common-header>
+    <div class="container mt-5">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import CommonHeader from './components/CommonHeader.vue';
+import CommonHeader from "./components/CommonHeader.vue";
 export default {
-    name: 'App',
-    components: {
-        CommonHeader
-    }
-}
+  name: "App",
+  components: {
+    CommonHeader
+  }
+};
 </script>
 
 <style>
