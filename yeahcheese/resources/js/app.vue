12277dd1394d62e3a20fce963eb500e8f1e23162
kage1219 <khhbsk15@gmail.com>
2020-05-15 16:05:31 +0900

    修正依頼対応

--- yeahcheese/resources/js/app.vue
+++ yeahcheese/resources/js/app.vue
@@ -7,13 +7,8 @@
 </template>
 
 <script>
-import Index from './pages/index'
-
 export default {
     name: 'App',
-    components: {
-        Index
-    }
 }
 </script>
 
--- yeahcheese/resources/js/pages/index.vue
+++ yeahcheese/resources/js/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mx-auto col-md-6">
-        <p>認証キー：<input type="text" class="form-control" v-model="authKey" @keyup.enter="findAuthKey(authKey)" placeholder="認証キーを入力してください"></p>
-        <button @click="findAuthKey(authKey)" class="btn btn-outline-primary">送信する</button>
+        <p>認証キー：<input type="text" class="form-control" v-model="authKey" @keyup.enter="findAuthKey()" placeholder="認証キーを入力してください"></p>
+        <button @click="findAuthKey()" class="btn btn-outline-primary">送信する</button>
     </div>
 </template>
 
@@ -14,7 +14,7 @@ export default {
     },
     methods: {
         findAuthKey() {  // ボタンを押した時に呼び出される
-            if(this.authKey === "") {
+            if(this.authKey.trim() === "") {
                 console.log("入力してください");
             }
             else console.log(this.authKey);
