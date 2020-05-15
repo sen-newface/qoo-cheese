241ad34097c4080bb9015cab868e8c5c6b298e66
kamata <kamata@SENPC-0616.local>
2020-05-15 11:22:23 +0900

    デフォルトのコメントを削除（サンプルコードは見直すかもなので残しておく）

---
+++ yeahcheese/tests/Feature/PhotoControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PhotoControllerTest extends TestCase
+{
+    // public function testExample()
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+}
