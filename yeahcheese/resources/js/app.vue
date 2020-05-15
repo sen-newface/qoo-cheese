9c612b1d762cfd0a22ef9f10f298b6e07d87b598
kamata <kamata@SENPC-0616.local>
2020-05-15 09:04:15 +0900

    ファイル名の変更に合わせて、取り込む側の名前も変更 + テストデータ追加

--- yeahcheese/resources/js/app.vue
+++ yeahcheese/resources/js/app.vue
@@ -1,36 +1,94 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <base-event-detail :event-info="event"></base-event-detail>
+    <base-event
+      v-for="event in events"
+      :key="event.id"
+      :event-info="event"
+    >
+    </base-event>
   </div>
 </template>
 
 <script>
-import BaseEventDetail from "./BaseEventDetail";
+import BaseEvent from "./BaseEvent";
 export default {
   name: "App",
   components: {
-    BaseEventDetail
+    BaseEvent
   },
   data() {
     return {
-      event: {
-        id: 1,
-        name: '運動会',
-        key: Math.random().toString(32).substring(2),
-        start_date: '2020/08/30',
-        end_date: '2020/09/5',
-        photos: [
-          {
-            id: 2,
-            name: '写真1'
-          },
-          {
-            id: 4,
-            name: '写真3'
-          }
-        ]
-      }
+      events: [
+        {
+          id: 1,
+          name: '運動会',
+          key: Math.random().toString(32).substring(2),
+          start_date: '2020/08/30',
+          end_date: '2020/09/5',
+          photos: [
+            {
+              id: 2,
+              name: '写真1'
+            },
+            {
+              id: 4,
+              name: '写真3'
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: '運動会',
+          key: Math.random().toString(32).substring(2),
+          start_date: '2020/08/30',
+          end_date: '2020/09/5',
+          photos: [
+            {
+              id: 2,
+              name: '写真1'
+            },
+            {
+              id: 4,
+              name: '写真3'
+            }
+          ]
+        },
+        {
+          id: 3,
+          name: '運動会',
+          key: Math.random().toString(32).substring(2),
+          start_date: '2020/08/30',
+          end_date: '2020/09/5',
+          photos: [
+            {
+              id: 2,
+              name: '写真1'
+            },
+            {
+              id: 4,
+              name: '写真3'
+            }
+          ]
+        },
+        {
+          id: 4,
+          name: '運動会',
+          key: Math.random().toString(32).substring(2),
+          start_date: '2020/08/30',
+          end_date: '2020/09/5',
+          photos: [
+            {
+              id: 2,
+              name: '写真1'
+            },
+            {
+              id: 4,
+              name: '写真3'
+            }
+          ]
+        }
+      ]
     }
   }
 };
