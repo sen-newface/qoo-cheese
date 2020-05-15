57c889ec5529913b6e5342f7860d2be6db8adb67
y-kazuya <y.kazuya.0124@au.com>
2020-05-15 12:51:27 +0900

    共通バリテーションメッセージ表示ファイルの作成

---
+++ yeahcheese/resources/js/components/validationMessages.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul v-if="errors.length" class="list-group mb-2">
+    <li v-for="er in errors" :key="er" class="list-group-item list-group-item-danger">・{{er}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    errors: {
+      type: Array,
+      default: () => []
+    }
+  }
+};
+</script>
