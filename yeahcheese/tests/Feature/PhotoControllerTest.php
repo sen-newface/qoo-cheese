19fefbb983983dbb8fabe5a172770a0d1d85f61a
kamata <kamata@SENPC-0616.local>
2020-05-15 11:36:30 +0900

    テストコントローラーの方にモデルをインポート＋動作確認ができたので、サンプルコードを削除

--- yeahcheese/tests/Feature/PhotoControllerTest.php
+++ yeahcheese/tests/Feature/PhotoControllerTest.php
@@ -5,13 +5,8 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Photo;
 
 class PhotoControllerTest extends TestCase
 {
-    // public function testExample()
-    // {
-    //     $response = $this->get('/');
-
-    //     $response->assertStatus(200);
-    // }
 }
