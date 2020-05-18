b7d09f4162b080fa895cc63eec3ccd24800125bc
y-kazuya <y.kazuya.0124@au.com>
2020-05-18 09:14:11 +0900

    ステータスをwatchして、エラ〜画面へ飛ばす

--- yeahcheese/resources/js/app.vue
+++ yeahcheese/resources/js/app.vue
@@ -9,10 +9,31 @@
 
 <script>
 import CommonHeader from "./components/CommonHeader.vue";
+import { mapGetters } from "vuex";
 export default {
   name: "App",
   components: {
     CommonHeader
+  },
+  computed: {
+    ...mapGetters({
+      code: "status/code"
+    })
+  },
+  watch: {
+    code: {
+      async handler(val) {
+        if (val === 500) {
+          this.$router.push("/500");
+        } else if (val === 404) {
+          this.$router.push("/404");
+        } else if (val === 403) {
+          this.$router.push("/403");
+        } else if (val === 403) {
+          this.$router.push("/login");
+        }
+      }
+    }
   }
 };
 </script>
