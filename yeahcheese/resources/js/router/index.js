7f47ed25ffb39accd665741d456c3a0e49a43f20
y-kazuya <y.kazuya.0124@au.com>
2020-05-18 09:16:17 +0900

    vuerouter マッチするURLがなかったら 404へ 、ルートガードの追加（ログイン判定）

--- yeahcheese/resources/js/router/index.js
+++ yeahcheese/resources/js/router/index.js
@@ -41,6 +41,10 @@ const routes = [
     path: '/register',
     component: Register,
     meta: { requiresNotAuth: true }
+  },
+  {
+    path: '*',
+    component: E404
   }
 ];
 
@@ -50,9 +54,15 @@ const router = new Router({
 });
 
 router.beforeEach((to, from, next) => {
-  // if (to.matched.some(record => record.meta.requiresNotAuth) && store.getters['users/isLogin']) {
-  //   next({ path: '/' });
-  // }
+  // meta: requiresNotAuth ログインしてたらダメ
+  if (to.matched.some(record => record.meta.requiresNotAuth) && store.getters['users/isLogin']) {
+    next({ path: '/' });
+  }
+
+  // meta: requiresAuth ログイン必要
+  if (to.matched.some(record => record.meta.requiresAuth) && !store.getters['users/isLogin']) {
+    next({ path: '/login' });
+  }
   next();
 });
 
