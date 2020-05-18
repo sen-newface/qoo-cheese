706de7d0a7b9bce766b37c223294a44099f2775b
kamata <kamata@SENPC-0616.local>
2020-05-18 11:18:04 +0900

    写真用フォームリクエスト定義

--- yeahcheese/app/Http/Requests/StorePhotoRequest.php
+++ yeahcheese/app/Http/Requests/StorePhotoRequest.php
@@ -3,28 +3,48 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StorePhotoRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
+
     public function authorize()
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
+        // ! vueで同じ名前を指定する
         return [
-            //
+            'event_id' => 'required|numeric|unique:photos',
+            'image_data' => 'required|image|mimes:jpeg|max:200'
         ];
     }
+
+    public function messages()
+    {
+        // ? event_idのバリデーションメッセージはいらない？
+        return [
+            'image_data.required' => ':attributeの選択は必須です',
+            'image_data.image' => '写真を指定してください',
+            'image_data.mimes' => ':attributeはjpegで指定してください',
+            'image_data.max' => ':max以下の:attributeを指定してください'
+        ]
+    }
+
+    protected function falidValidation(Validator $validator)
+    {
+        $data = [
+            'data' => [],
+            'status' => 'error',
+            'summary' => 'Failed validation.',
+            'errors' => $validator->errors()->toArray(),
+        ];
+        throw new HttpResponseException(response()->json($data, 422));
+    }
+
+
+
 }
