e4be5c56a6fe0b56f4a1091e6c8e83da4a29aaf5
y-kazuya <y.kazuya.0124@au.com>
2020-05-18 15:07:03 +0900

    store index APIの作成

--- yeahcheese/app/Http/Controllers/Api/EventsController.php
+++ yeahcheese/app/Http/Controllers/Api/EventsController.php
@@ -10,62 +10,61 @@ use App\Http\Resources\Event as EventResource;
 
 class EventsController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:sanctum')
-            ->except(['auth', 'show']);
-    }
+  public function __construct()
+  {
+    $this->middleware('auth:sanctum')
+      ->except(['auth', 'show']);
+  }
 
-    /**
-     * イベント一覧取得
-     */
-    public function index()
-    {
-        // TODO: ユーザーに紐づくイベントを複数取得
-        // return EventResource::collection($events);
-    }
+  /**
+   * イベント一覧取得
+   */
+  public function index(Request $request)
+  {
+    return EventResource::collection($request->user()->events);
+  }
 
-    /**
-     * 認証キーと紐づくイベント取得
-     */
-    public function auth()
-    {
-        // TODO: トークンを利用して認証キーをあれこれ照合して、認証キーに紐づくイベントを取得し、リソースクラスに渡す
-        // return new EventResource($event);
-    }
+  /**
+   * 認証キーと紐づくイベント取得
+   */
+  public function auth()
+  {
+    // TODO: トークンを利用して認証キーをあれこれ照合して、認証キーに紐づくイベントを取得し、リソースクラスに渡す
+    // return new EventResource($event);
+  }
 
-    /**
-     * 単一イベント取得
-     */
-    public function show(Event $event)
-    {
+  /**
+   * 単一イベント取得
+   */
+  public function show(Event $event)
+  {
 
-        //
-    }
-    public function store(StoreEventRequest $request)
-    {   
-        $request->merge(['user_id' => $request->user()->id]);
-        
-        return EventResource::make(Event::create($request->toArray()));
-    }
+    //
+  }
+  public function store(StoreEventRequest $request)
+  {
+    $request->merge(['user_id' => $request->user()->id]);
 
-    /**
-     * イベント情報更新（写真を除く）
-     */
-    public function update(Event $event)
-    {
-        // TODO: イベントを更新してから、そのインスタンスをリソースクラスに渡す
-        // return new EventResource($event);
-    }
+    return EventResource::make(Event::create($request->toArray()));
+  }
 
-    /**
-     * イベント削除
-     */
-    public function destroy(Event $event): array
-    {
-        // TODO: イベント削除の処理
-        return [
-            'status' => 204
-        ];
-    }
+  /**
+   * イベント情報更新（写真を除く）
+   */
+  public function update(Event $event)
+  {
+    // TODO: イベントを更新してから、そのインスタンスをリソースクラスに渡す
+    // return new EventResource($event);
+  }
+
+  /**
+   * イベント削除
+   */
+  public function destroy(Event $event): array
+  {
+    // TODO: イベント削除の処理
+    return [
+      'status' => 204
+    ];
+  }
 }
