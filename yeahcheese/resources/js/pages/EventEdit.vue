3f768f91a9a51ec70d73c2a9f22a233935339557
kamata <kamata@SENPC-0616.local>
2020-05-19 09:14:45 +0900

    コメント追加

--- yeahcheese/resources/js/pages/EventEdit.vue
+++ yeahcheese/resources/js/pages/EventEdit.vue
@@ -1,12 +1,21 @@
 <template>
     <div id="event-edit">
-        
     </div>
 </template>
 
 <script>
+/**
+ * [イベント編集] - イベント情報の更新と写真の追加・削除が可能
+ * * 必要な情報
+ *  * 単一のイベント情報
+ *   * created内でstoreにあるeventsからルートパラメータのイベントを取得
+ *   * 更新時に参照するため、eventに格納
+ *  * イベントに紐づく写真情報
+ *   * イベントのリレーションから取得
+ *   * 削除時に参照するため、photosに格納
+ */
 export default {
-    name: 'EventEdit',   
+    name: 'EventEdit',
 }
 </script>
 
