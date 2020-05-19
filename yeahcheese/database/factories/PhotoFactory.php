2f6f8595b831239f7645ebc80792431664c64a94
y-kazuya <y.kazuya.0124@au.com>
2020-05-19 13:53:54 +0900

    photo ファクトリー修正 画像パスを返す様に

--- yeahcheese/database/factories/PhotoFactory.php
+++ yeahcheese/database/factories/PhotoFactory.php
@@ -4,8 +4,8 @@ use App\Photo;
 use Faker\Generator as Faker;
 
 $factory->define(Photo::class, function (Faker $faker) {
-    return [
-        'image_path' => $faker->url,
-        'event_id' => $faker->numberBetween(1, 10)
-    ];
+  return [
+    'image_path' => $faker->imageUrl,
+    'event_id' => $faker->numberBetween(1, 10)
+  ];
 });
