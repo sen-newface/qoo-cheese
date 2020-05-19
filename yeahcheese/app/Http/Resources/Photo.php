15f28610c0d62d1dedb230b41468aae2599a334c
y-kazuya <y.kazuya.0124@au.com>
2020-05-19 13:53:23 +0900

    リソース修正

--- yeahcheese/app/Http/Resources/Photo.php
+++ yeahcheese/app/Http/Resources/Photo.php
@@ -6,17 +6,17 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class Photo extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return [
-            'id' => $this->id,
-            'image_path' => $this->image_path
-        ];
-    }
+  /**
+   * Transform the resource into an array.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return array
+   */
+  public function toArray($request)
+  {
+    return [
+      'id' => $this->id,
+      'image_path' => $this->image_path
+    ];
+  }
 }
