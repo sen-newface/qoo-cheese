2723b79f26827548e587d6d484dfbb31ec3faa01
y-kazuya <y.kazuya.0124@au.com>
2020-05-19 13:59:11 +0900

    debug 消し

--- yeahcheese/resources/js/store/events/getters.js
+++ yeahcheese/resources/js/store/events/getters.js
@@ -6,13 +6,11 @@ const getters = {
     return state.events.find(event => event.id == id)
   },
   isMyEventByEventId: (state, getters) => (event_id) => {
-    let user = store.getters["users/user"]
-    if (!user.name) return false
-    var target = getters.getEventForId(event_id)
-    console.log(target)
-    console.log(user.id)
-    if (target && target.user_id == user.id) return true
-    return false
+    let user = store.getters["users/user"];
+    if (!user.name) return false;
+    var target = getters.getEventForId(event_id);
+    if (target && target.user_id == user.id) return true;
+    return false;
   },
 }
 export default getters
