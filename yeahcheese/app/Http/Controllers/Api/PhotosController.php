6b8886c50fd877e2dd59b3ee04a900890e447555
y-kazuya <y.kazuya.0124@au.com>
2020-05-19 13:53:01 +0900

    photos index APIの作成

--- yeahcheese/app/Http/Controllers/Api/PhotosController.php
+++ yeahcheese/app/Http/Controllers/Api/PhotosController.php
@@ -5,32 +5,50 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Photo;
+use App\Event;
 use App\Http\Resources\Photo as PhotoResource;
 
 class PhotosController extends Controller
 {
-    /**
-     * イベントに紐づく写真の追加
-     *
-     * @return void
-     */
-    public function store()
-    {
-        // TODO: Vueから受け取ったevent_idを含む、新規に保存したPhotoインスタンスをリソースクラスに渡す
-        // return new PhotoResource($photo);
-    }
 
-    /**
-     * イベントに紐づく写真の削除
-     *
-     * @param App\Photo
-     * @return void
-     */
-    public function delete(Photo $photo)
-    {
-        // TODO: イベントに紐づく写真削除の処理
-        return [
-            'status' => 204
-        ];
-    }
+  public function __construct()
+  {
+    $this->middleware('auth:sanctum')
+      ->except(['index']);
+  }
+
+  /**
+   * イベントに紐づく写真の取得
+   *
+   * @return void
+   */
+  public function index(Event $event)
+  {
+    // TODO: Vueから受け取ったevent_idを含む、新規に保存したPhotoインスタンスをリソースクラスに渡す
+    return response(PhotoResource::collection($event->photos, 200));
+  }
+  /**
+   * イベントに紐づく写真の追加
+   *
+   * @return void
+   */
+  public function store()
+  {
+    // TODO: Vueから受け取ったevent_idを含む、新規に保存したPhotoインスタンスをリソースクラスに渡す
+    // return new PhotoResource($photo);
+  }
+
+  /**
+   * イベントに紐づく写真の削除
+   *
+   * @param App\Photo
+   * @return void
+   */
+  public function delete(Photo $photo)
+  {
+    // TODO: イベントに紐づく写真削除の処理
+    return [
+      'status' => 204
+    ];
+  }
 }
