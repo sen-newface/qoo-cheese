89ec99623da0f8ebb66012a94b3bd4c78ad14a70
kamata <kamata@SENPC-0616.local>
2020-05-20 10:14:49 +0900

    vuexの修正

--- yeahcheese/resources/js/store/photos/actions.js
+++ yeahcheese/resources/js/store/photos/actions.js
@@ -1,4 +1,16 @@
 import * as types from '../mutation-types';
+import api from "../../api";
+import store from "../../store";
 
 export default {
+  async postPhoto({ commit }, { id, data }) {
+    const response = await api.eventPhotosPost(id, data);
+    const isSuccess = store.getters["status/isApiSuccess"];
+    if (isSuccess) {
+      // commit("appendPhoto", response);
+      return response;
+    } else {
+      return response.errors;
+    }
+  }
 }
