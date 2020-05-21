706755a008b92a89b26d0ad49272b240ef3922bc
takahashikazatake <hiroyuki.takahashi@sencorp.co.jp>
2020-05-21 14:34:54 +0900

    やっちゃだめなことたくさんやってみた

--- yeahcheese/app/Event.php
+++ yeahcheese/app/Event.php
@@ -38,4 +38,119 @@ class Event extends Model
     protected $fillable = [
     'name', 'start_date', 'end_date', 'user_id'
     ];
+
+    // 使用してないメソッド、戻り値おかしい, めっちゃ長いメソッド
+    private function no(): array
+    {
+        // インデント異常, 不使用変, 少数文字
+          $a = [];
+    $bbb = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // ifの後スペースなし、閉じかっこの後スペースなし、複雑すぎる
+        if(true) {
+            if (true){
+                if (true) {
+                    if (true) {
+                        if (true) {
+                            if (true) {
+                                if (true) {
+                                    if (true) {
+                                        if (true) {
+                                            if (true) {
+                                                if (true) {
+                                                    if (true) {
+                                                        if (true) {
+                                                            if (true) {
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // カンマのあとスペースなし
+        $cc = [1,2];
+    }
 }
