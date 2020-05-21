273233bbf59d675b7cc8a3203c06fd17e1b2fae9
kage1219 <khhbsk15@gmail.com>
2020-05-21 16:06:44 +0900

    ソートボタンの実装

--- yeahcheese/resources/js/pages/eventsIndex.vue
+++ yeahcheese/resources/js/pages/eventsIndex.vue
@@ -1,7 +1,25 @@
 <template>
   <div>
     <router-link class="btn btn-outline-info mb-5" :to="{ path: '/events/new'}">新規作成</router-link>
-    <event-list v-show="events.length" v-for="event in events" :key="event.key" :eventInfo="event"></event-list>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <button
+          class="btn btn-outline-secondary"
+          type="button"
+          :value="sortText"
+          @click="changeSortText"
+        >{{ sortText }}</button>
+      </div>
+      <select class="custom-select" id="inputGroupSelect03" v-model="selected">
+        <option v-for="op in options" :key="op.text" :value="op.num">{{op.text}}</option>
+      </select>
+    </div>
+    <event-list
+      v-show="events.length"
+      v-for="event in showEvents"
+      :key="event.key"
+      :eventInfo="event"
+    ></event-list>
     <p v-show="!events.length">イベントは一つも登録されていません。</p>
   </div>
 </template>
@@ -11,10 +29,45 @@ import eventList from "../components/BaseEvent";
 import { mapGetters } from "vuex";
 export default {
   components: { eventList },
+  data() {
+    return {
+      sortText: "昇順",
+      changeSort: false,
+      selected: "1",
+      options: [
+        { text: "イベント作成順(既定)", num: "1" },
+        { text: "イベント名順", num: "2" },
+        { text: "公開開始日順", num: "3" }
+      ]
+    };
+  },
   computed: {
     ...mapGetters({
-      events: "events/events"
-    })
+      events: "events/events",
+      eventsSortById: "events/eventsSortById",
+      eventsSortByName: "events/eventsSortByName",
+      eventsSortByStartDate: "events/eventsSortByStartDate"
+    }),
+    showEvents() {
+      switch (this.selected) {
+        case "1":
+          return this.eventsSortById(this.changeSort);
+        case "2":
+          return this.eventsSortByName(this.changeSort);
+        case "3":
+          return this.eventsSortByStartDate(this.changeSort);
+        default:
+          return this.events;
+      }
+    }
+  },
+  methods: {
+    changeSortText() {
+      this.changeSort = !this.changeSort;
+      this.sortText === "昇順"
+        ? (this.sortText = "降順")
+        : (this.sortText = "昇順");
+    }
   },
   async created() {
     await this.$store.dispatch("events/initGetEvents");
--- yeahcheese/resources/js/store/events/getters.js
+++ yeahcheese/resources/js/store/events/getters.js
@@ -1,7 +1,30 @@
-
 import store from '../../store'
 const getters = {
-  events: state => state.events,
+  events: state => state.events.slice().sort(function (a, b) { return (a.id > b.id ? 1 : -1) }),
+  eventsSortById: state => (change_sort) => {
+    return state.events.slice().sort(function (a, b) {
+      if (change_sort) {
+        return (a.id < b.id ? 1 : -1)
+      }
+      return (a.id > b.id ? 1 : -1)
+    })
+  },
+  eventsSortByName: state => (change_sort) => {
+    return state.events.slice().sort(function (a, b) {
+      if (change_sort) {
+        return (a.name < b.name ? 1 : -1)
+      }
+      return (a.name > b.name ? 1 : -1)
+    })
+  },
+  eventsSortByStartDate: state => (change_sort) => {
+    return state.events.slice().sort(function (a, b) {
+      if (change_sort) {
+        return (a.start_date < b.start_date ? 1 : -1)
+      }
+      return (a.start_date > b.start_date ? 1 : -1)
+    })
+  },
   initLoad: state => state.initLoad,
   getEventForId: state => (id) => {
     return state.events.find(event => event.id == id)
