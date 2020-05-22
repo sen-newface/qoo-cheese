ecd8b72c39ce9ba394e94321cf23eb22bc95a364
y-kazuya <y.kazuya.0124@au.com>
2020-05-22 10:14:40 +0900

    ページネイトのためのgettersの追加

--- yeahcheese/resources/js/store/events/getters.js
+++ yeahcheese/resources/js/store/events/getters.js
@@ -1,8 +1,20 @@
 
 import store from '../../store'
 const getters = {
-  events: state => state.events,
+  events: state => state.events.slice().sort(function (a, b) { return (a.id < b.id ? 1 : -1) }),
+  last_page: (state, getters) => {
+    let page = Math.floor(state.events.length / getters.page_per_events);
+    let add = state.events.length % getters.page_per_events;
+    return add ? page + 1 : page
+  },
+  page_per_events: state => state.page_per_events,
+  curretEventPage: state => state.curretEventPage,
   initLoad: state => state.initLoad,
+  getEventsForPageId: (state, getters) => (page = 1) => {
+    let end_index = getters.page_per_events * page
+    let start_index = end_index - getters.page_per_events
+    return state.events.slice(start_index, end_index)
+  },
   getEventForId: state => (id) => {
     return state.events.find(event => event.id == id)
   },
