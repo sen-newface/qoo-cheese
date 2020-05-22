a562d59ea30cc52a3963abd8dd6ebdc9492ea6b6
y-kazuya <y.kazuya.0124@au.com>
2020-05-22 10:15:15 +0900

    pagenatieのためのmutation

--- yeahcheese/resources/js/store/events/mutations.js
+++ yeahcheese/resources/js/store/events/mutations.js
@@ -2,20 +2,30 @@ import * as types from '../mutation-types';
 
 export default {
   setEvents(state, events) {
-    state.events = events
+    state.events = state.events.concat(events);
   },
+
+  setCurretEventPage(state, page) {
+    state.curretEventPage = parseInt(page)
+  },
+  setNumberOfPage(state, num) {
+    state.page_per_events = num
+  },
+
   setInitLoad(state, bool) {
     state.initLoad = bool
   },
+
   resetEvent(state) {
     state.events = []
     state.initLoad = true
+    state.last_page = 1
+    state.curretEventPage = 1
   },
   setEvent(state, event) {
-    state.events.push(event)
+    state.events.unshift(event)
   },
-
   delEvents(state) {
     state.events = []
-  }
+  },
 }
