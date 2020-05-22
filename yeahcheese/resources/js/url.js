e622c3ed2af0763079f1bb6e0527617c6343d5bf
kamata <kamata@SENPC-0616.local>
2020-05-22 16:05:58 +0900

    パスのスラッシュを書き忘れていたので、修正

--- yeahcheese/resources/js/url.js
+++ yeahcheese/resources/js/url.js
@@ -5,10 +5,10 @@ export default {
   EVENTS_INDEX: '/api/events',
   EVENTS_STORE: '/api/events',
   EVENTS_AUTH: '/api/events/auth',
-  EVENTS_SHOW: (id, key) => '/api/events/' + id + '?key=' + key,
+  EVENTS_SHOW: (id, key) => '/api/events/' + id + "?key=" + key,
   EVENTS_UPDATE: (id) => '/api/events/' + id,
   EVENTS_DESTROY: (id) => '/api/events/' + id,
-  PHOTOS_INDEX: (id) => '/api/events/' + id + 'photos',
-  PHOTOS_STORE: (id) => '/api/events/' + id + 'photos',
-  PHOTOS_DESTROY: (event_id, photo_id) => '/api/events/' + event_id + 'photos/' + photo_id
+  PHOTOS_INDEX: (id) => '/api/events/' + id + '/photos',
+  PHOTOS_STORE: (id) => '/api/events/' + id + '/photos',
+  PHOTOS_DESTROY: (event_id, photo_id) => '/api/events/' + event_id + '/photos/' + photo_id
 }
