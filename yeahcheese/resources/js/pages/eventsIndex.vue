5055854ecf28ca51d13bb486219e2b19c998b24c
kage1219 <khhbsk15@gmail.com>
2020-05-25 09:05:22 +0900

    イベントカードの公開または非公開の色分け

--- yeahcheese/resources/js/pages/eventsIndex.vue
+++ yeahcheese/resources/js/pages/eventsIndex.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <router-link class="btn btn-outline-info mb-5" :to="{ path: '/events/new'}">新規作成</router-link>
-    <event-list v-show="events.length" v-for="event in events" :key="event.key" :eventInfo="event"></event-list>
+    <event-list
+      v-show="events.length"
+      v-for="event in events"
+      :class="colorClassByDate(event.start_date, event.end_date)"
+      :key="event.key"
+      :eventInfo="event"
+    ></event-list>
     <p v-show="!events.length">イベントは一つも登録されていません。</p>
   </div>
 </template>
@@ -16,8 +22,35 @@ export default {
       events: "events/events"
     })
   },
+  methods: {
+    colorClassByDate: function(start_date, end_date) {
+      let today = this.getToday();
+      return {
+        "border-color": start_date <= today && today <= end_date,
+        "border border-danger": !(start_date <= today && today <= end_date)
+      };
+    },
+    getToday() {
+      let now = new Date();
+      let y = now.getFullYear();
+      let m = now.getMonth() + 1;
+      let d = now.getDate();
+      if (m < 10) {
+        m = "0" + m;
+      }
+      if (d < 10) {
+        d = "0" + d;
+      }
+      return y + "-" + m + "-" + d;
+    }
+  },
   async created() {
     await this.$store.dispatch("events/initGetEvents");
   }
 };
 </script>
+<style scoped>
+.border-color {
+  border-color: #66ccff;
+}
+</style>
