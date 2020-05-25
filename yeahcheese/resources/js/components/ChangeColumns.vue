eaea02213a2be7d9f70bd180f6a66908fc4786a1
kamata <kamata@SENPC-0616.local>
2020-05-25 09:41:24 +0900

    eventShow.vueで定義したコンポーネントファイルの雛形作成

---
+++ yeahcheese/resources/js/components/ChangeColumns.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="change-columns">
+    <div v-for="idx in (min, max)" :key="idx" class="form-check form-check-inline">
+      <input
+        class="form-check-input"
+        type="radio"
+        name="inlineRadioOptions"
+        :id="radioId(idx)"
+        :value="idx"
+        @change="changeColumn"
+      />
+      <label class="form-check-label" :for="radioId(idx)">{{ idx }}</label>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapMutation, mapMutations } from "vuex";
+export default {
+  name: "ChangeColumns",
+  props: {
+    min: {
+      type: Number,
+      default: 2
+    },
+    max: {
+      type: Number,
+      default: 5
+    }
+  },
+  methods: {
+    ...mapMutations("display", ["changeColumn"])
+  }
+};
+</script>
+
+<style scoped>
+</style>
