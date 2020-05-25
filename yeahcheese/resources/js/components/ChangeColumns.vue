9f3c08ecf32832d886301a98a691419d0ceec133
kamata <kamata@SENPC-0616.local>
2020-05-25 10:04:24 +0900

    受け取るプロパティの追加

--- yeahcheese/resources/js/components/ChangeColumns.vue
+++ yeahcheese/resources/js/components/ChangeColumns.vue
@@ -20,12 +20,24 @@ export default {
   name: "ChangeColumns",
   props: {
     min: {
+      // * 表示する列数の最小値
       type: Number,
       default: 2
     },
     max: {
+      // * 最大何列まで表示できるかの情報
       type: Number,
       default: 5
+    },
+    device: {
+      // * アクセスしてきたデバイスの種類（未使用）
+      type: Boolean,
+      default: true
+    },
+    dispStyle: {
+      // * 何列で表示するかの情報
+      type: Number,
+      default: 2
     }
   },
   methods: {
