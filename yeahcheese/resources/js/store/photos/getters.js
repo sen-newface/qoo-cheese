8187ad6436c8a89368efbf311598397bd7925e5e
kamata <kamata@SENPC-0616.local>
2020-05-26 09:40:21 +0900

    修正依頼対応：削除し切れていなかった部分を削除

--- yeahcheese/resources/js/store/photos/getters.js
+++ yeahcheese/resources/js/store/photos/getters.js
@@ -2,11 +2,6 @@ const getters = {
   getPhotosForEvnetId: state => (id) => {
     const obj = state.eventPhotos.find(event => event.event_id == id.toString())
     return obj ? obj.photos : null
-  },
-  getNumberOfPhotosForEventId: (state, getters, rootState) => (id) => {
-    const events = rootState.events.events;
-    const event = events.filter(event => event.id == id.toString())[0];
-    return event.photos_count;
   }
 }
 export default getters
