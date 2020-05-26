90259b74ad644a7a53dd68dc79257e6553b72cbe
kamata <kamata@SENPC-0616.local>
2020-05-26 09:30:07 +0900

    （ユーザーのログイン状態の確認 + イベント認証キーの確認 + 公開期限内か否かの確認 + ログイン状態によって返却するリクエストデータを変化させる）ようなミドルウェアを作成（まだ試してない）

--- yeahcheese/app/Http/Middleware/AuthenticateEvent.php
+++ yeahcheese/app/Http/Middleware/AuthenticateEvent.php
@@ -3,18 +3,97 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
+use App\Event;
+use Carbon\Carbon;
 
 class AuthenticateEvent
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
     public function handle($request, Closure $next)
     {
+        $user = Auth::user();
+        $key = $request->key;
+        $event = Event::where('key', $key)->first();
+        $request = $this->getRequest($request, $user, $event);
         return $next($request);
+    }
+    /**
+     * ユーザーの種類とイベントの有無に応じて、レスポンス内容を変化させて返却
+     */
+    private function getRequest($request, $user, $event)
+    {
+        $request = $this->isValidkeyOrInvalidKey($request, $user, $event);
+        return $request;
+    }
+
+    /**
+     * 有効な認証キーか無効な認証キーかによってレスポンス内容を変化させて返却
+     */
+    private function isValidkeyOrInvalidKey($request, $user, $event)
+    {
+        $opt = null;
+        if (is_null($event)) {
+            $opt = [
+                'errors' => ['認証キーが間違っています'],
+                'status' => 406,
+                'key' => $request->key
+            ];
+            $request->merge($opt);
+        } else {
+            $request = $this->withinResponseOrOutsideResponse($request, $user, $event);
+        }
+        return $request;
+    }
+
+    /**
+     * 公開期限内か公開期限外かによってレスポンス内容を変化させて返却
+     */
+    private function withinResponseOrOutsideResponse($request, $user, $event)
+    {
+        if ($this->checkDeadline($event)) {
+            $path = $this->getPath($user, $event);
+            $opt = [
+                'errors' => null,
+                'status' => 200,
+                'event' => $event,
+                'path' => $path,
+            ];
+            $request->merge($opt);
+        } else {
+            $opt = [
+                'errors' => ['公開期限外のイベントです'],
+                'status' => 403,
+                'key' => $request->key,
+            ];
+            $request->merge($opt);
+        }
+        return $request;
+    }
+
+    /**
+     * 取得できたイベントが公開期限内であるかどうかの判定
+     */
+    private function checkDeadline($event)
+    {
+        $today = Carbon::now();
+        $start_date = $event->start_date;
+        $end_date = $event->end_date;
+        dd($today, $start_date, $end_date);
+        $within = $today->between($start_date, $end_date);
+        return $within;
+    }
+
+    /**
+     * ユーザーの種類に応じて、認証後の遷移先パスを変化させて返却
+     */
+    private function getPath($user, $event)
+    {
+        $path = null;
+        if (is_null($user)) {
+            $path = '/event/' . $event->id;
+        } else {
+            $path = '/event/' . $event->id;
+        }
+        return $path;
     }
 }
