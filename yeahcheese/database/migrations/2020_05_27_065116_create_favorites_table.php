2712413061a78c847cf6b8b825d02bed882b188b
kage1219 <khhbsk15@gmail.com>
2020-05-27 15:59:13 +0900

    お気に入りテーブルのマイグレーション作成

---
+++ yeahcheese/database/migrations/2020_05_27_065116_create_favorites_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'favorites',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('user_id');
+                $table->integer('photo_id');
+                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+                $table->foreign('photo_id')->references('id')->on('photos')->onDelete('cascade');
+                $table->timestamps();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
