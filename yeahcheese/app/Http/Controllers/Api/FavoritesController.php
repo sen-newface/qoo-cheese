4c6826d2a832b66e2a862888caf9d60369cf5724
kage1219 <khhbsk15@gmail.com>
2020-05-28 12:23:04 +0900

    テストの修正

--- yeahcheese/app/Http/Controllers/Api/FavoritesController.php
+++ yeahcheese/app/Http/Controllers/Api/FavoritesController.php
@@ -24,11 +24,16 @@ class FavoritesController extends Controller
     public function store(Request $request)
     {
         $user = auth('sanctum')->user();
-        $favorite = new Favorite();
-        $favorite->user_id = $user->id;
-        $favorite->photo_id = $request->photo_id;
+        $target = Favorite::where('user_id', intval($user->id))
+            ->where('photo_id', intval($request->photo_id))->get();
+        if ($target->isEmpty()) {
+            $favorite = new Favorite();
+            $favorite->user_id = $user->id;
+            $favorite->photo_id = $request->photo_id;
 
-        return response(FavoriteResource::make(Favorite::create($favorite->toArray())), 201);
+            return response(FavoriteResource::make(Favorite::create($favorite->toArray())), 201);
+        }
+        return response(null, 200);
     }
 
     public function destroy(Request $request)
